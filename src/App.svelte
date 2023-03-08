8c285933e12b700bdc9d7f352ccf5d6cf68f1663
emu <hi@emu.media>
2023-03-08 10:20:40 -0500

    :shrug:

--- src/App.svelte
+++ src/App.svelte
@@ -8,8 +8,10 @@
       event.preventDefault()
       print()
     }
+  }
 
-    console.log(this.selectionStart)
+  function handleInput(event) {
+    text = this.value
   }
 
   async function print() {
@@ -17,7 +19,7 @@
 
     if (!trimmedText) return
 
-    const res = await fetch('http://192.168.1.151:3000', {
+    const res = await fetch('/api/print', {
       method: 'POST',
       mode: 'cors',
       headers: {
@@ -30,34 +32,11 @@
     text = ''
   }
 
-  function limitLineLength(str, maxLength) {
-    // Split the string into words
-    const words = str.split(' ');
-
-    let currentLineLength = 0;
-    let result = '';
-
-    // Iterate through the words
-    for (const word of words) {
-      // If adding the current word to the line would exceed the max length, start a new line
-      if (currentLineLength + word.length > maxLength) {
-        result += '\n';
-        currentLineLength = 0;
-      }
-
-      // Add the word to the line
-      result += word + ' ';
-      currentLineLength += word.length + 1;
-    }
-
-    return result;
-  }
-
 </script>
 
 <main>
-  <textarea bind:value={ text } on:keydown={ handleKeydown }></textarea>
-  <div class="submission"><button on:click={ print }>Print Text</button><span class="hint">Shift + Enter to print</span></div>
+  <textarea value={ text } on:keydown={ handleKeydown } on:input={ handleInput }></textarea>
+  <div class="submission"><button on:click={ print }>Print&nbsp;Text&nbsp;&nbsp;🖨️</button></div>
 </main>
 
 <style>
